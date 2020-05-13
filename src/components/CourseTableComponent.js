95829bbdda619b9243065041334a5275aad09713
Andrew Cabey <me@acabey.xyz>
2020-05-13 14:00:40 -0400

    Copied CourseTableComponent from template

--- src/components/CourseTableComponent.js
+++ src/components/CourseTableComponent.js
@@ -1,8 +1,211 @@
-"use strict";
-import React;
-import {Component} from "react";
+import React from 'react'
 
-class CourseTableComponent extends Component {
-}
+const CourseTableComponent = ({}) =>
 
-export default CourseTableComponent;
+    <div className="row justify-content-center">
+        <div className="col-md-10 col-12">
+            <table className="table table-striped">
+                <thead>
+                <tr className="table-dark">
+                    <th className="wbdv-header wbdv-title">Title</th>
+                    <th className="wbdv-header wbdv-owner d-none d-md-table-cell">Owned</th>
+                    <th className="wbdv-header wbdv-last-modified d-none d-md-table-cell">Modified</th>
+                    <th>
+                        <i className="fa fa-sort-alpha-asc wbdv-header wbdv-sort"></i>
+                        <i className="fa fa-th wbdv-button wbdv-grid-layout"></i>
+                        <i className="d-none fa fa-th wbdv-button wbdv-list-layout"></i>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course active">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                <tr className="wbdv-row wbdv-course">
+                    <td>
+                        <i className="fa fa-file-text wbdv-row wbdv-icon"></i>
+                        <a href="/course-editor/course-editor.template.client.html">CS4550</a>
+                    </td>
+                    <td className="wbdv-row wbdv-owner d-none d-md-table-cell">JGA</td>
+                    <td className="wbdv-row wbdv-modified-date d-none d-md-table-cell">Today</td>
+                    <td>
+                        <button className="float-right btn btn-danger wbdv-row wbdv-button wbdv-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+                </tbody>
+                <tfoot>
+
+                </tfoot>
+            </table>
+        </div>
+    </div>;
+
+export default CourseTableComponent
