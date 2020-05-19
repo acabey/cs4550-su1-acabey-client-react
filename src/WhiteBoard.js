091e1ad09c3575f8df4c6ac8ebb8eaf43ba4cda7
Andrew Cabey <me@acabey.xyz>
2020-05-19 12:02:26 -0400

    Renamed App to WhiteBoard as highest level of abstraction

---
+++ src/WhiteBoard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Whiteboard.css';
+import CourseManagerContainer from "./containers/CourseManagerContainer";
+
+function WhiteBoard() {
+  return <CourseManagerContainer/>
+}
+
+export default WhiteBoard;
