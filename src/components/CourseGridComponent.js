9640cf387fdc141d7bddf9ddc3ecee97ebc5538c
Andrew Cabey <me@acabey.xyz>
2020-05-26 22:42:09 -0400

    Refactored grid head to separate component

--- src/components/CourseGridComponent.js
+++ src/components/CourseGridComponent.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import CourseGridCardComponent from "./CourseGridCardComponent";
 import CourseListComponent from "./CourseListComponent";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faFolder, faList, faSortAlphaDown, faSortAlphaUp} from "@fortawesome/free-solid-svg-icons";
+import CourseGridHeadComponent from "./CourseGridHeadComponent";
 
 /**
  * xs | 1
@@ -15,27 +14,10 @@ class CourseGridComponent extends CourseListComponent {
 
     render = () =>
         <div>
-            <div className="row justify-content-center">
-                <div className="col-4">
-                    <h5>Recent Documents</h5>
-                </div>
-                <div className="col-4">
-                    <h5>Owned by me</h5>
-                </div>
-                <div className="col-2">
-                    <button className={"btn wbdv-btn"}
-                            onClick={() => this.props.toggleLayout()}>
-                        <FontAwesomeIcon icon={faList} size={'1x'}/>
-                    </button>
-                    <button className={"btn wbdv-btn"}
-                            onClick={() => this.toggleSort()}>
-                        <FontAwesomeIcon icon={sortState === "AZ_DESC" ? faSortAlphaDown : faSortAlphaUp} size={'1x'}/>
-                    </button>
-                    <button className={"btn wbdv-btn"}>
-                        <FontAwesomeIcon icon={faFolder} size={'1x'}/>
-                    </button>
-                </div>
-            </div>
+
+            <CourseGridHeadComponent sortState={this.state.sortDescription}
+                                     toggleSort={this.toggleSort}
+                                     toggleLayout={this.props.toggleLayout}/>
 
             <div className="ml-2 mr-2">
                 <div className="row">
