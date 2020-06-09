1a6e8eb0aa6bf8b43f7f3ce5e0cc9db0f936629d
Andrew Cabey <me@acabey.xyz>
2020-06-09 03:32:45 -0400

    Implemented module and topic reducers based off starter code

--- src/reducers/lessonReducer.js
+++ src/reducers/lessonReducer.js
@@ -6,22 +6,34 @@ const initialState = {
     ]
 };
 
-const lessonReducer = (state=initialState, action) => {
-    switch(action.type) {
+const lessonReducer = (state=initialState, event) => {
+    switch(event.type) {
+        case "CREATE_LESSON":
+            return {
+                ...state,
+                lessons: [...state.lessons, event.newLesson]
+            };
         case "DELETE_LESSON":
             return {
                 ...state,
-                lessons: state.lessons.filter(lesson => lesson._id !== action.lessonId)
+                lessons: state.lessons.filter(lesson => lesson._id !== event.lessonId)
+            };
+        case "UPDATE_LESSON":
+            return {
+                ...state,
+                lessons: state.lessons.map(
+                    lesson => lesson._id === event.updatedLesson._id ?
+                        event.updatedLesson : lesson )
+            };
+        case "FIND_LESSON":
+            return {
+                ...state,
+                lessons: state.lessons.filter(lesson => lesson._id === event.lessonId)
             };
         case "FIND_LESSONS_FOR_MODULE":
             return {
                 ...state,
-                lessons: action.actualLessons
-            };
-        case "CREATE_LESSON":
-            return {
-                ...state,
-                lessons: [...state.lessons, action.newLesson]
+                lessons: event.actualLessons
             };
         default:
             return state
