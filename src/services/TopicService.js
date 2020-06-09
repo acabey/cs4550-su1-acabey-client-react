6af20f69cdbc5738160f6109219805cabc0d7f29
Andrew Cabey <me@acabey.xyz>
2020-06-09 02:49:34 -0400

    TopicService stub code

---
+++ src/services/TopicService.js
@@ -0,0 +1,17 @@
+const url = 'https://wbdv-generic-server.herokuapp.com/api';
+const nuid = '001867846';
+
+export const createTopic = (lessonId, topic) =>
+    null
+
+export const findTopicsForLesson = (lessonId) =>
+    null
+
+export const findTopic = (topicId) =>
+    null
+
+export const updateTopic = (topicId, topic) =>
+    null
+
+export const deleteTopic= (topicId) =>
+    null
