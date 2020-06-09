ef3231888fa4bc246aefe058015a3a2855129af0
Andrew Cabey <me@acabey.xyz>
2020-06-09 02:40:11 -0400

    Implemented lessonService. Refactored to Class

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -1,19 +1,75 @@
-export const findLessonsForModule = (moduleId) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/${moduleId}/lessons`)
-        .then(response => response.json())
+"use strict";
+class LessonServiceClient {
 
-export const createLesson = (moduleId, newLesson) =>
-    fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/modules/${moduleId}/lessons`, {
-        method: 'POST',
-        body: JSON.stringify(newLesson),
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
-        .then(response => response.json())
+    url = 'https://wbdv-generic-server.herokuapp.com/api';
+    nuid = '001867846';
 
-export const deleteLesson = (lessonId) => {}
+    /**
+     * retrieves all lessons for course whose ID is moduleId
+     * @param moduleId
+     * @returns {Promise<T>}
+     */
+    findLessonsForModule = (moduleId) =>
+        fetch(`${this.url}/${this.nuid}/modules/${moduleId}/lessons`)
+            .then(response => response.json())
 
-export default {
-  findLessonsForModule
+    /**
+     * creates a new lesson instance for the module whose ID is moduleId
+     * @param moduleId
+     * @param newLesson
+     * @returns {Promise<T>}
+     */
+    createLesson = (moduleId, newLesson) =>
+        fetch(`${this.url}/${this.nuid}/modules/${moduleId}/lessons`,
+            {
+                method: 'POST',
+                body: JSON.stringify(newLesson),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+
+    /**
+     * removes lesson whose ID is lessonId
+     * @param lessonId
+     * @returns {Promise<T>}
+     */
+    deleteLesson = (lessonId) =>
+        fetch(`${this.url}/${this.nuid}/lessons/${lessonId}`,
+            {
+                method: 'DELETE',
+            })
+            .then(response => response.json())
+
+
+    /**
+     * retrieves one lesson whose ID is lessonId (optional)
+     * @param lessonId
+     * @returns {Promise<T>}
+     */
+    findLesson = (lessonId) =>
+        fetch(`${this.url}/${this.nuid}/lessons/${lessonId}`,
+            {
+                method: 'GET',
+            })
+            .then(response => response.json())
+
+
+    /**
+     * updates one lesson whose ID is lessonId
+     * @param lessonId
+     * @param lesson
+     * @returns {Promise<T>}
+     */
+    updateLesson = (lessonId, lesson) =>
+        fetch(`${this.url}/${this.nuid}/lessons/${lessonId}`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(lesson),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+            .then(response => response.json())
 }
