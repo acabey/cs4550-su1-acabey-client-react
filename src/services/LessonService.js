1eef0559f07a6e017a990aad1a83bc31e9f1911e
Andrew Cabey <me@acabey.xyz>
2020-06-09 02:43:17 -0400

    Refactored back to non class

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -1,75 +1,71 @@
-"use strict";
-class LessonServiceClient {
+const url = 'https://wbdv-generic-server.herokuapp.com/api';
+const nuid = '001867846';
 
-    url = 'https://wbdv-generic-server.herokuapp.com/api';
-    nuid = '001867846';
+/**
+ * retrieves all lessons for course whose ID is moduleId
+ * @param moduleId
+ * @returns {Promise<T>}
+ */
+export const findLessonsForModule = (moduleId) =>
+    fetch(`${url}/${nuid}/modules/${moduleId}/lessons`)
+        .then(response => response.json())
 
-    /**
-     * retrieves all lessons for course whose ID is moduleId
-     * @param moduleId
-     * @returns {Promise<T>}
-     */
-    findLessonsForModule = (moduleId) =>
-        fetch(`${this.url}/${this.nuid}/modules/${moduleId}/lessons`)
-            .then(response => response.json())
+/**
+ * creates a new lesson instance for the module whose ID is moduleId
+ * @param moduleId
+ * @param newLesson
+ * @returns {Promise<T>}
+ */
+export const createLesson = (moduleId, newLesson) =>
+    fetch(`${url}/${nuid}/modules/${moduleId}/lessons`,
+        {
+            method: 'POST',
+            body: JSON.stringify(newLesson),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(response => response.json())
 
-    /**
-     * creates a new lesson instance for the module whose ID is moduleId
-     * @param moduleId
-     * @param newLesson
-     * @returns {Promise<T>}
-     */
-    createLesson = (moduleId, newLesson) =>
-        fetch(`${this.url}/${this.nuid}/modules/${moduleId}/lessons`,
-            {
-                method: 'POST',
-                body: JSON.stringify(newLesson),
-                headers: {
-                    'content-type': 'application/json'
-                }
-            })
-            .then(response => response.json())
-
-    /**
-     * removes lesson whose ID is lessonId
-     * @param lessonId
-     * @returns {Promise<T>}
-     */
-    deleteLesson = (lessonId) =>
-        fetch(`${this.url}/${this.nuid}/lessons/${lessonId}`,
-            {
-                method: 'DELETE',
-            })
-            .then(response => response.json())
+/**
+ * removes lesson whose ID is lessonId
+ * @param lessonId
+ * @returns {Promise<T>}
+ */
+export const deleteLesson = (lessonId) =>
+    fetch(`${url}/${nuid}/lessons/${lessonId}`,
+        {
+            method: 'DELETE',
+        })
+        .then(response => response.json())
 
 
-    /**
-     * retrieves one lesson whose ID is lessonId (optional)
-     * @param lessonId
-     * @returns {Promise<T>}
-     */
-    findLesson = (lessonId) =>
-        fetch(`${this.url}/${this.nuid}/lessons/${lessonId}`,
-            {
-                method: 'GET',
-            })
-            .then(response => response.json())
+/**
+ * retrieves one lesson whose ID is lessonId (optional)
+ * @param lessonId
+ * @returns {Promise<T>}
+ */
+export const findLesson = (lessonId) =>
+    fetch(`${url}/${nuid}/lessons/${lessonId}`,
+        {
+            method: 'GET',
+        })
+        .then(response => response.json())
 
 
-    /**
-     * updates one lesson whose ID is lessonId
-     * @param lessonId
-     * @param lesson
-     * @returns {Promise<T>}
-     */
-    updateLesson = (lessonId, lesson) =>
-        fetch(`${this.url}/${this.nuid}/lessons/${lessonId}`,
-            {
-                method: 'PUT',
-                body: JSON.stringify(lesson),
-                headers: {
-                    'content-type': 'application/json'
-                }
-            })
-            .then(response => response.json())
-}
+/**
+ * updates one lesson whose ID is lessonId
+ * @param lessonId
+ * @param lesson
+ * @returns {Promise<T>}
+ */
+export const updateLesson = (lessonId, lesson) =>
+    fetch(`${url}/${nuid}/lessons/${lessonId}`,
+        {
+            method: 'PUT',
+            body: JSON.stringify(lesson),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(response => response.json())
