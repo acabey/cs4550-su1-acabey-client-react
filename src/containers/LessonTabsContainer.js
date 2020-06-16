802e6be006d25609d2ba00aabeb6d8af3dc01fdd
Andrew Cabey <me@acabey.xyz>
2020-06-15 23:44:20 -0400

    Implemented Lesson container from module list

---
+++ src/containers/LessonTabsContainer.js
@@ -0,0 +1,64 @@
+import {connect} from "react-redux";
+import LessonListComponent from "../components/LessonListComponent";
+import lessonReducer from "../reducers/lessonReducer";
+import LessonService from "../services/LessonService";
+
+const stateToPropertyMapper = (state, ownProps) => {
+    return {
+        lessons: state.lessonReducer.lessons,
+        selectedLessonId: state.lessonReducer.selectedLessonId,
+        params: ownProps.params,
+    }
+};
+
+const dispatchToPropertyMapper = (dispatch) => {
+    return {
+        findLessonsForModule: (moduleId) =>  {
+            LessonService.findLessonsForModule(moduleId)
+                .then(lessonsForModule => dispatch({
+                    type: 'FIND_LESSONS_FOR_MODULE',
+                    lessons: lessonsForModule
+                }))
+        },
+        findAllLessons: () => {
+            LessonService.findAllLessons()
+                .then(actualLessons => dispatch({
+                    type: 'FIND_ALL_LESSONS',
+                    lessons: actualLessons
+                }))
+        },
+        updateLesson: (lessonId, newLessonData) => {
+            LessonService.updateLesson(lessonId, newLessonData)
+                .then(status => dispatch({
+                    type: 'UPDATE_LESSON',
+                    updatedLesson: newLessonData
+                }))
+        },
+        createLesson: (moduleId, newLesson) => {
+            LessonService.createLesson(moduleId, newLesson)
+                .then(actualNewLesson => dispatch({
+                    type: "CREATE_LESSON",
+                    newLesson: actualNewLesson
+                }))
+        },
+        deleteLesson: (lessonId) => {
+            LessonService.deleteLesson(lessonId)
+                .then(status => dispatch({
+                    type: "DELETE_LESSON",
+                    lessonId: lessonId
+                }))
+        },
+        selectLesson: (lessonId) => {
+            dispatch({
+                type: "SELECT_LESSON",
+                lessonId: lessonId
+            })
+        }
+    }
+};
+
+const LessonListContainer = connect
+(stateToPropertyMapper, dispatchToPropertyMapper)
+(LessonListComponent);
+
+export default LessonListContainer
